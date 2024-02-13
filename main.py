c98c4d504645e8f7628a0dab22761f28d1998a03
Nathaniela Aptanta Parama <nathan@jkt1.ebdesk.com>
2024-02-13 17:17:07 +0700

    feat: adding time exec

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
+import traceback
 from loguru import logger
 from config import kafka_config
 from service.pg_service import PgService
 from config.base import settings
+import time
 
 TOPIC = settings.MP_KAFKA_TOPIC
+GROUP_ID = settings.MP_KAFKA_GROUP_ID
 
 
 def fetch(message_lists: list):
@@ -26,12 +29,14 @@ if __name__ == "__main__":
                 messages = consumer.poll(1.0)
 
                 for _, message_list in messages.items():
+                    start_time = time.time()
                     fetch(message_list)
-
                     logger.info("=======================================")
-                    logger.info(f"consumer {TOPIC} {len(message_list)}")
+                    logger.info(f"consumer {TOPIC} {GROUP_ID} {len(message_list)}")
+                    logger.info(f"Time Exec: {time.time() - start_time}")
                     logger.info("=======================================")
         except Exception as err:
+            traceback.print_exc()
             raise err
     except KeyboardInterrupt:
         print("Interrupt Keyboard")
